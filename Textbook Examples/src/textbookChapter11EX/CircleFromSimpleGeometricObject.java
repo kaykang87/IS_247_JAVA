97c1e33bf099b71824845a71b8d9358f9f36685e
kkang1119 <kkang1119@gmail>
2016-09-11 20:05:15 -0400

    Geometric Example

--- Textbook Examples/src/textbookChapter11EX/CircleFromSimpleGeometricObject.java
+++ Textbook Examples/src/textbookChapter11EX/CircleFromSimpleGeometricObject.java
@@ -3,5 +3,51 @@ package textbookChapter11EX;
 /**
  * Created by kkang on 9/10/2016.
  */
-public class CircleFromSimpleGeometricObject {
+public class CircleFromSimpleGeometricObject extends SimpleGeometricObject {
+
+    private double radius;
+
+    public CircleFromSimpleGeometricObject()
+    {
+    }
+
+    public CircleFromSimpleGeometricObject(double radius) {
+        this.radius = radius;
+    }
+
+    public CircleFromSimpleGeometricObject(double radius, String color, boolean filled){
+        this.radius = radius;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    /** return radius */
+    public double getRadius(){
+        return radius;
+    }
+
+    /** Set a new radius */
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    /** Return Area */
+    public double getArea(){
+        return radius = radius * Math.PI;
+    }
+
+    /** Return Diamater */
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    /** Return perimeter */
+    public double getPerimeter(){
+        return 2 * radius * Math.PI;
+    }
+
+    /** Print the circle info */
+    public void printCircle() {
+        System.out.println("The circle is created " + getDateCreated() + "  and the radius is " + radius);
+    }
 }
--- Textbook Examples/src/textbookChapter11EX/TestCricleRectangle.java
+++ Textbook Examples/src/textbookChapter11EX/TestCricleRectangle.java
@@ -3,5 +3,20 @@ package textbookChapter11EX;
 /**
  * Created by kkang on 9/10/2016.
  */
-public class TestCricleRectangle {
+public class TestCricleRectangle
+{
+    public static void main(String args[])
+    {
+        CircleFromSimpleGeometricObject circle = new CircleFromSimpleGeometricObject(1);
+        System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getDiameter());
+
+        RectangleFromSimpleGeometricObject rectangle = new RectangleFromSimpleGeometricObject(2, 4);
+        System.out.println("\nA rectangle " + rectangle.toString());
+        System.out.println("The area is " + rectangle.getArea());
+        System.out.println("The perimeter is " + rectangle.getPerimeter());
+    }
 }
