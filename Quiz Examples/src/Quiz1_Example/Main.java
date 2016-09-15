172c4e0453d938239acb71dd092eec8ecc4dbe4e
kkang1119 <kkang1119@gmail>
2016-09-15 11:47:41 -0400

    Added Quiz 1 Example

---
+++ Quiz Examples/src/Quiz1_Example/Main.java
@@ -0,0 +1,28 @@
+package Quiz1_Example;
+
+public class Main
+{
+    public static void main(String[] args){
+        new B();
+    }
+}
+
+class A{
+    int i =7;
+    public A(){
+        System.out.println("i form A is " + i);
+    }
+    public void setI(int i){
+        this.i = 2 * i;
+    }
+}
+
+class B extends A{
+    public B(){
+        setI(20);
+        System.out.println("i from B is " + i);
+    }
+    public void setI(int i ){
+        this.i = 3 * i;
+    }
+}
