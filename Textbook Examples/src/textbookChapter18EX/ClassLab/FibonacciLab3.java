eea111e01e2e14dcff9e1cdd84be17dd7a6c6eb7
kkang1119 <kkang1119@gmail.com>
2016-10-06 20:35:54 -0400

    Added Class lab
    Factorial Recursion and Fibonicci Tail Recursion

---
+++ Textbook Examples/src/textbookChapter18EX/ClassLab/FibonacciLab3.java
@@ -0,0 +1,36 @@
+package textbookChapter18EX.ClassLab;
+
+import java.util.Scanner;
+
+/**
+ * Created by kkang on 10/6/2016.
+ */
+public class FibonacciLab3 {
+    public static long fib1(long n)
+    {
+        if (n == 0){
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        } else {
+            return fib1(0, 1, n);
+        }
+    }
+
+    private static long fib1(long a, long b, long n) {
+        if (n <= 0) {
+            return a;
+        }
+        return fib1(b, a + b, n - 1);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an index for a Fibonacci number: ");
+        int n = input.nextInt();
+
+        // Find and display the Fibonacci number
+        System.out.println("The Fibonacci number at index "
+                + n + " is " + fib1(n));
+    }
+}
