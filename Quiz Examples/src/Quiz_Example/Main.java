67d0f5b3a55c7e4d60527fcdb0c97951590602ec
kkang1119 <kkang1119@gmail.com>
2016-10-13 14:42:28 -0400

    a

---
+++ Quiz Examples/src/Quiz_Example/Main.java
@@ -0,0 +1,20 @@
+package Quiz_Example;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
+
+class A extends B{
+    A(String s){
+        super(s);
+    }
+}
+
+class B{
+    public B(String s){
+    }
+}
+
+
--- Textbook Examples/src/textbookChapter18EX/ClassLab/FibonacciLab2.java
+++ Textbook Examples/src/textbookChapter18EX/ClassLab/FibonacciLab2.java
@@ -1,40 +1,36 @@
 package textbookChapter18EX.ClassLab;
 import java.util.Scanner;
-
 /**
  * Created by kyong kang on 10/6/2016.
  * Rewriting "ComputeFibonacci" class using tail recursion
  */
 public class FibonacciLab2 {
-    //The method for finding the Fibonacci number
-    public static long fib(long index){
-        if (index == 0) {
-            return 0;
-        } else if (index == 1) // Base case
-        {
-            return 1;
-        }else{
-        return fib(index, 0,1);
-    }}
 
-    public static long fib(long index, long a ,long b) {
-        if (index == 0) {
-            return a;
-        } else // Reduction and recursive calls
-        {
-            return fib(index-1, b , a+b);
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter an index for a Fibonacci number: ");
         int index = input.nextInt();
 
-        // Find and display the Fibonacci number
-        System.out.println("The Fibonacci number at index "
-                + index + " is " + fib(index));
+        System.out.println("The Fibonacci number of the index you entered is: ");
+        System.out.println(fib(index));
     }
 
+    public static long fib(int index){
+        if (index == 0){
+            return 0;
+        } else if (index == 1){
+            return 1;
+        } else{
+            return fib(index,0,1);
+        }
+    }
+
+    public static long fib(int index, long a, long b){
+        if (index == 0){
+            return a;
+        } else{
+            return fib(index-1, b, a+b);
+        }
+    }
 }
 
