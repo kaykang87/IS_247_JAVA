9b0d2c63dc6a4d16ded86278b6e2da61984b1b8c
kkang1119 <kkang1119@gmail.com>
2016-10-31 17:27:18 -0400

    Removed testing GIT

--- Extra Credit(Even_Odd_Alphabets)/src/testingGIT.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kkang on 10/31/2016.
- */
-public class testingGIT {
-    public static void main(String[] args){
-        // testing to see if it push
-        // Is push different from commit?
-    }
-}
