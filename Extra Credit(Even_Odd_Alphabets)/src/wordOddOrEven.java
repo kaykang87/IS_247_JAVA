b2f295d888fcc433fd552769ae2828853f144b3b
kkang1119 <kkang1119@gmail>
2016-10-31 13:55:54 -0400

    Added additional comments

--- Extra Credit(Even_Odd_Alphabets)/src/wordOddOrEven.java
+++ Extra Credit(Even_Odd_Alphabets)/src/wordOddOrEven.java
@@ -2,6 +2,12 @@
  * Kyong Kang
  * IS 247
  * Extra Credit. Is the word Odd or Even?
+ * Solution: ODD # of Vowels in a word will always return the word as ODD
+ * because there will always be ODD left
+ * and
+ * Even + ODD = ODD
+ * Even # of Vowels in a word will always return the word as EVEN because
+ * ODD + ODD = EVEN
  */
 
 import java.util.*;
@@ -16,7 +22,7 @@ public class wordOddOrEven {
         System.out.println("This program will figure out if the word is Even or Odd");
         System.out.println("Please enter a single word. Only in alphabets");
 
-        while (!input.hasNext("[A-Za-z]+")) {
+        while (!input.hasNext("[A-Za-z]+")) {                           // While loop to make users only input Alphabets
             System.out.println("Enter only ALPHABETS");
             input.next();
         }
@@ -26,8 +32,8 @@ public class wordOddOrEven {
 
         for (char c : charArray) {                                      // Creates a for loop. Store charArray into char 'c'
             if ("aeiouAEIOU".indexOf(c) >= 0) {                         // Compare user input to condition string "aeiouAEIOU"
-                ++x;                                                    // increment x if matching char is found
-            }
+                ++x;                                                    // pre-increment x if matching char is found
+            }                                                           // ++ post-increment gives same result
         }
         if (x % 2 == 0) {                                               // If matching vowel is even #, it returns 0
             System.out.println(evenOdd[0]);                             // Prints out String Array [0] "Even"
