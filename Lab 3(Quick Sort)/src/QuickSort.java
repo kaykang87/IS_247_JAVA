0e082c769e14a0bcb048bcb1df4ec004f6bba456
kkang1119 <kkang1119@gmail.com>
2016-11-20 22:09:24 -0500

    Finished Lab 3. QuickSorting using median of 3.

--- Lab 3(Quick Sort)/src/QuickSort.java
+++ Lab 3(Quick Sort)/src/QuickSort.java
@@ -1,75 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Kyong Kang 11/17/16 Lab
  * Revise the QuickSort to select middle element as the pivot
  * Exercise 23.4
  */
 public class QuickSort {
-  public static void quickSort(int[] list) {
-    quickSort(list, 0, list.length - 1);
-  }
-
-  private static void quickSort(int[] list, int first, int last) {
-    if (last > first) {
-      int pivotIndex = partition(list, first, last);
-      quickSort(list, first, pivotIndex - 1);
-      quickSort(list, pivotIndex + 1, last);
-    }
-  }
-
-  /** Partition the array list[first..last] */
-  private static int partition(int[] list, int first, int last) {
-    int middle = list[(list.length -1) / 2];  // to hold the middle value
-    int pivot = median(first, middle, last); // Choose the median element as the pivot
-    int low = first + 1; // Index for forward search
-    int high = last; // Index for backward search
-
-
-    while (high > low) {
-      // Search forward from left
-      while (low <= high && list[low] <= pivot)
-        low++;
-
-      // Search backward from right
-      while (low <= high && list[high] > pivot)
-        high--;
-
-      // Swap two elements in the list
-      if (high > low) {
-        int temp = list[high];
-        list[high] = list[low];
-        list[low] = temp;
-      }
-    }
-
-    while (high > first && list[high] >= pivot)
-      high--;
-
-    // Swap pivot with list[high]
-    if (pivot > list[high]) {
-      list[first] = list[high];
-      list[high] = pivot;
-      return high;
-    }
-    else {
-      return first;
-    }
-  }
-
-  /** A test method */
   public static void main(String[] args) {
-    int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
-    quickSort(list);
-    for (int i = 0; i < list.length; i++)
-      System.out.print(list[i] + " ");
+    int[] intArray = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+    for (int i =0; i < intArray.length ; i++){
+      System.out.print(intArray[i] + " ");
+    }
+    System.out.println();
+    quickSort(intArray);
+    for (int i =0; i < intArray.length ; i++){
+      System.out.print(intArray[i] + " ");
+    }
   }
-  //Method to return median
-  public static int median(int first, int middle, int last) {
-    return Math.max(Math.min(first, middle), Math.min(Math.max(first, middle),last));
+
+  public static void quickSort(int[] intArray) {
+    quickSort(intArray, 0, intArray.length - 1);
+  }
+
+  public static void quickSort(int[] intArray, int left, int right) {
+    int size = right - left + 1;
+    if (size <= 3)
+      manualSort(intArray, left, right);
+    else {
+      double median = medianOf3(intArray, left, right);
+      int partition = partitionIt(intArray, left, right, median);
+      quickSort(intArray, left, partition - 1);
+      quickSort(intArray, partition + 1, right);
+    }
+  }
+
+  public static int medianOf3(int[] intArray, int left, int right) {
+    int center = (left + right) / 2;
+
+    if (intArray[left] > intArray[center])
+      swap(intArray, left, center);
+
+    if (intArray[left] > intArray[right])
+      swap(intArray, left, right);
+
+    if (intArray[center] > intArray[right])
+      swap(intArray, center, right);
+
+    swap(intArray, center, right - 1);
+    return intArray[right - 1];
+  }
+
+  public static void swap(int[] intArray, int dex1, int dex2) {
+    int temp = intArray[dex1];
+    intArray[dex1] = intArray[dex2];
+    intArray[dex2] = temp;
+  }
+
+  public static int partitionIt(int[] intArray, int left, int right, double pivot) {
+    int leftPtr = left;
+    int rightPtr = right - 1;
+
+    while (true) {
+      while (intArray[++leftPtr] < pivot)
+        ;
+      while (intArray[--rightPtr] > pivot)
+        ;
+      if (leftPtr >= rightPtr)
+        break;
+      else
+        swap(intArray, leftPtr, rightPtr);
+    }
+    swap(intArray, leftPtr, right - 1);
+    return leftPtr;
+  }
+
+  public static void manualSort(int[] intArray, int left, int right) {
+    int size = right - left + 1;
+    if (size <= 1)
+      return;
+    if (size == 2) {
+      if (intArray[left] > intArray[right])
+        swap(intArray, left, right);
+      return;
+    } else {
+      if (intArray[left] > intArray[right - 1])
+        swap(intArray, left, right - 1);
+      if (intArray[left] > intArray[right])
+        swap(intArray, left, right);
+      if (intArray[right - 1] > intArray[right])
+        swap(intArray, right - 1, right);
+    }
   }
 }
